7e97636c843cfaa67242ed4afc30d2310308c716
Jaron <147181586+JaronWenger@users.noreply.github.com>
2024-10-03 21:41:45 -0500

    Time: 62 ms (14.18%), Space: 49 MB (50.35%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    
+let k = nums1.length - 1;
+
+while (m > 0 && n > 0){
+    if (nums1[m-1] > nums2[n-1]){
+        nums1[k--] = nums1[m-1];
+        m--;
+    } else {
+        nums1[k--] = nums2[n-1];
+        n--;
+    }
+}
+
+while (n > 0){
+
+    nums1[k--] = nums2[n-1];
+    n--;
+}
+
+};
