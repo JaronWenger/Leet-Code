d24fc7bdf2d55b8930ea191b247153e9aa2c0fce
Jaron <147181586+JaronWenger@users.noreply.github.com>
2025-01-04 11:23:28 -0600

    Time: 1 ms, Memory: 59 MB - LeetHub

---
+++ 32162-1229-121-best-time-to-buy-and-sell-stock/32162-1229-121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+
+    let min = Infinity;
+    let maxProfit = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+        min = Math.min(min, prices[i]);
+        maxProfit = Math.max(maxProfit, prices[i]-min);
+    }
+
+    return maxProfit;
+
+};
+
+
+
+
+
+
+
+
+
+
+
